51472838c9e729d8a5e55cf9d738aad97466ca04
ivannhf <34865616+ivannhf@users.noreply.github.com>
2018-02-27 00:49:02 +0800

    GNSS Server

---
+++ src/Server.java
@@ -0,0 +1,112 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+	ServerSocket serverSocket;
+	Socket socket;
+	BufferedInputStream in;
+	DataInputStream d;
+
+	public Server() {
+		try {
+			socket = new Socket();
+			socket.setReuseAddress(true);
+			serverSocket = new ServerSocket();
+			serverSocket.setReuseAddress(true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void start() {
+
+		Runnable r = new Runnable() {
+			public void run() {
+				try {
+					while (true) {
+						serverSocket = new ServerSocket(8080);
+						System.out.println("Server running at: 8080");
+						socket = serverSocket.accept();
+						System.out.println("Server accept");
+
+						in = new BufferedInputStream(socket.getInputStream());
+						System.out.println("buffer stream open");
+						d = new DataInputStream(in);
+						String fileName = d.readUTF();
+						Files.copy(d, Paths.get("E:\\Windows\\Desktop\\test\\" + fileName));
+						System.out.println("file finish " + fileName);
+
+						in.reset();
+						d.reset();
+						//socket.close();
+						//serverSocket.close();
+
+						/*if (!TCP.started) {
+							d.close();
+							in.close();
+							socket.close();
+							serverSocket.close();
+							break;
+						}*/
+
+						/*
+						 * InputStream in = new
+						 * BufferedInputStream(socket.getInputStream());
+						 * 
+						 * OutputStream out = new FileOutputStream
+						 * ("E:\\Windows\\Desktop\\test\\test.txt"); byte[]
+						 * bytes = new byte[2048];
+						 * 
+						 * int count; while ((count = in.read(bytes)) > 0) {
+						 * out.write(bytes, 0, count); }
+						 * System.out.println("jpg finish");
+						 * 
+						 * out.close(); in.close(); socket.close();
+						 * serverSocket.close();
+						 */
+
+						/*
+						 * inputStreamReader = new
+						 * InputStreamReader(socket.getInputStream());
+						 * bufferedReader = new
+						 * BufferedReader(inputStreamReader); message =
+						 * bufferedReader.readLine();
+						 * 
+						 * System.out.println(message);
+						 * 
+						 * inputStreamReader.close(); bufferedReader.close();
+						 * serverSocket.close(); socket.close();
+						 */
+					}
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		};
+
+		new Thread(r).start();
+	}
+
+	public void stop() {
+		try {
+			//in.close();
+			//d.close();
+			socket.close();
+			serverSocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/TCP.java
@@ -0,0 +1,187 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class TCP extends JFrame {
+
+	private JPanel contentPane;
+
+	private static ServerSocket serverSocket;
+	private static Socket socket;
+	private static BufferedReader bufferedReader;
+	private static InputStreamReader inputStreamReader;
+	private static String message = "";
+	private static String path = "";
+
+	private static int bytesRead;
+	public static int PORT = 0;
+	int current = 0;
+	private static JTextField port;
+	private static JButton btnStartServer, btnStopServer;
+
+	public static Boolean started = false;
+
+	static Server server;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {					
+					TCP frame = new TCP();
+					frame.setVisible(true);
+
+					//server = new Server();
+					//Server();
+					//bkServer();
+					
+
+					btnStartServer.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							// TODO Auto-generated method stub
+							System.out.println(Integer.parseInt(port.getText()));
+							if ((port.getText() != "") && (Integer.parseInt(port.getText()) > 0)) {
+								started = true;
+								server = new Server();
+								PORT = Integer.parseInt(port.getText());
+								port.setEnabled(!started);
+								btnStartServer.setVisible(!started);
+								btnStopServer.setVisible(started);
+								server.start();
+							}
+						}
+					});
+
+					btnStopServer.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							// TODO Auto-generated method stub
+							started = false;
+							port.setEnabled(!started);
+							btnStartServer.setVisible(!started);
+							btnStopServer.setVisible(started);
+							server.stop();
+							server = null;
+						}
+					});
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TCP() {
+		setTitle("GNSS Recorder Server");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JLabel lblServer = new JLabel("Server");
+		lblServer.setBounds(10, 10, 46, 15);
+		contentPane.add(lblServer);
+
+		port = new JTextField();
+		port.setBounds(81, 45, 96, 21);
+		contentPane.add(port);
+		port.setColumns(10);
+
+		JLabel lblServerPort = new JLabel("Server Port");
+		lblServerPort.setBounds(10, 48, 74, 15);
+		contentPane.add(lblServerPort);
+
+		btnStartServer = new JButton("Start Server");
+		btnStartServer.setBounds(10, 136, 108, 23);
+		contentPane.add(btnStartServer);
+
+		btnStopServer = new JButton("Stop Server");
+		btnStopServer.setBounds(10, 136, 108, 23);
+		btnStopServer.setVisible(false);
+		contentPane.add(btnStopServer);
+	}
+
+	private static void bkServer() {
+		try {
+			while (true) {
+				serverSocket = new ServerSocket(8080);
+				System.out.println("Server running at: " + serverSocket.getLocalPort());
+				socket = serverSocket.accept();
+
+				BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
+				DataInputStream d = new DataInputStream(in);
+				String fileName = d.readUTF();
+				Files.copy(d, Paths.get("E:\\Windows\\Desktop\\test\\" + fileName));
+				System.out.println("file finish " + fileName);
+
+				in.close();
+				d.close();
+				socket.close();
+				serverSocket.close();
+
+				/*
+				 * InputStream in = new
+				 * BufferedInputStream(socket.getInputStream());
+				 * 
+				 * OutputStream out = new FileOutputStream
+				 * ("E:\\Windows\\Desktop\\test\\test.txt"); byte[] bytes = new
+				 * byte[2048];
+				 * 
+				 * int count; while ((count = in.read(bytes)) > 0) {
+				 * out.write(bytes, 0, count); }
+				 * System.out.println("jpg finish");
+				 * 
+				 * out.close(); in.close(); socket.close();
+				 * serverSocket.close();
+				 */
+
+				/*
+				 * inputStreamReader = new
+				 * InputStreamReader(socket.getInputStream()); bufferedReader =
+				 * new BufferedReader(inputStreamReader); message =
+				 * bufferedReader.readLine();
+				 * 
+				 * System.out.println(message);
+				 * 
+				 * inputStreamReader.close(); bufferedReader.close();
+				 * serverSocket.close(); socket.close();
+				 */
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
